602ae3bc9819f433e19ec119b51a8e87a278cce7
Sheetal Naik <sheetalnaik848@gmail.com>
2025-03-27 23:43:28 +0530

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TodoItem from './TodoItem'
 import TodoFilterControl from './TodoFilterControl'
 
-const TodoList = () => {
+const TodoList = ({ todos, setTodos, filteredTodos, filterStatus, setFilterStatus }) => {
+
+    const [leftTodoCount, setLeftTodoCount] = useState(0);
+
+    useEffect(() => {
+        const unCompletedTodos = todos.filter((todo) => !todo.completed);
+        setLeftTodoCount(unCompletedTodos.length);
+    }, [todos]);
+
+    const textPlacer = filterStatus === "completed" ? "closed task" : "task";
+
+    const clearCompletedTodos = () => {
+        setTodos(todos.filter((todo) => !todo.completed));
+        setFilterStatus("all");
+    }
+
   return (
     <>
-    <div>TodoList</div>
-    <TodoItem />
-    <TodoFilterControl />
+    <section className="todo-list-section">
+        {
+            filteredTodos.length < 1 ? (
+                <p className="info-text">There is no {textPlacer}</p>
+            ) : (
+                <ul className="todo-list">
+                    {
+                        filteredTodos.map((todo) => (
+                            <TodoItem 
+                                todo={todo}
+                                key={todo.id}
+                                setTodos={setTodos}
+                                todos={todos}
+                                />
+                        ))
+                    }
+                </ul>
+            )
+        }
+
+        <div className="todo-filter-control">
+            <div className="todos-count">{leftTodoCount} items left</div>
+            <div className="control-btn group filter-control-for-desktop">
+                <TodoFilterControl 
+                    filterStatus={filterStatus}
+                    setFilterStatus={setFilterStatus}
+                 />
+            </div>
+        </div>
+
+        <div className="control-btn">
+            <button className="btn" onClick={clearCompletedTodos}>Clear completed</button>
+        </div>
+
+    </section>
+
+    {/* For Mobile */}
+    <section className="filter-control-for-mobile">
+        <div className="control-btn group">
+        <TodoFilterControl 
+            filterStatus={filterStatus}
+            setFilterStatus={setFilterStatus}
+        />
+        </div>
+    </section>
     </>
   )
 }
