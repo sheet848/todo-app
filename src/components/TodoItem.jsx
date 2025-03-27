3adab3bda276c55df33bb8bb4234ec9a07c95c07
Sheetal Naik <sheetalnaik848@gmail.com>
2025-03-27 23:42:51 +0530

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import IconCheck from '../assets/icon-check.svg'
 
-const TodoItem = () => {
+const TodoItem = ({ todo, todos, setTodos }) => {
+
+  const [mutableTodo, setMutableTodo] = useState(todo);
+
+  const classes = mutableTodo.completed ? "completed" : "";
+
+  const checkIcon = mutableTodo.completed ? (
+    <img src={IconCheck} alt="Completed" />
+  ) : ( "" );
+
+  const toggleCompleted = () => {
+    setMutableTodo({...mutableTodo, completed: !mutableTodo.completed});
+
+    const updatedTodos = todos.map((item) =>
+      item.id === todo.id ? {...item, completed: !item.completed } : item
+    );
+
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div>TodoItem</div>
+    <>
+    <li className={classes}>
+      <label htmlFor={`todoCheckbox-${todo.id}`}>Completed Checkbox</label>
+      <input type="checkbox"
+             id={`todoCheckbox-${todo.id}`}
+             name='completed-checkbox'
+             defaultChecked={mutableTodo.completed} 
+      />
+      <div className="checkbox-border-wrap">
+        <span className="checkbox" onClick={toggleCompleted}>{checkIcon}</span>
+      </div>
+      <p>{mutableTodo.content}</p>
+    </li>
+    </>
   )
 }
 
